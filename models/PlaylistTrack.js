a6127199f3703d5c4bf113eefdff0a9cb7bc6cb0
kenu <kenu@okjsp.pe.kr>
2024-04-01 09:45:55 +0900

    mariadb

--- models/PlaylistTrack.js
+++ models/PlaylistTrack.js
@@ -1,52 +1,47 @@
 const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('PlaylistTrack', {
-    PlaylistId: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'Playlist',
-        key: 'PlaylistId'
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define(
+    'PlaylistTrack',
+    {
+      PlaylistId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true, // Only one field should be autoIncrement
+        primaryKey: true, // This field is the primary key
+        references: {
+          model: 'Playlist',
+          key: 'PlaylistId'
+        },
+        unique: true
       },
-      unique: true
-    },
-    TrackId: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'Track',
-        key: 'TrackId'
+      TrackId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Track',
+          key: 'TrackId'
+        }
       }
+    }, {
+      sequelize,
+      tableName: 'PlaylistTrack',
+      timestamps: false,
+      indexes: [
+        {
+          name: 'sqlite_autoindex_PlaylistTrack_1',
+          unique: true,
+          fields: [{ name: 'PlaylistId' }, { name: 'TrackId' }]
+        },
+        {
+          name: 'IPK_PlaylistTrack',
+          unique: true,
+          fields: [{ name: 'PlaylistId' }, { name: 'TrackId' }]
+        },
+        {
+          name: 'IFK_PlaylistTrackTrackId',
+          fields: [{ name: 'TrackId' }]
+        }
+      ]
     }
-  }, {
-    sequelize,
-    tableName: 'PlaylistTrack',
-    timestamps: false,
-    indexes: [
-      {
-        name: "sqlite_autoindex_PlaylistTrack_1",
-        unique: true,
-        fields: [
-          { name: "PlaylistId" },
-          { name: "TrackId" },
-        ]
-      },
-      {
-        name: "IPK_PlaylistTrack",
-        unique: true,
-        fields: [
-          { name: "PlaylistId" },
-          { name: "TrackId" },
-        ]
-      },
-      {
-        name: "IFK_PlaylistTrackTrackId",
-        fields: [
-          { name: "TrackId" },
-        ]
-      },
-    ]
-  });
+  );
 };
